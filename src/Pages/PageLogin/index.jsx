aeb434ed6285a6048797e6568030add879303bb0
Marcio Martins <mmarciom1martins635@gmail.com>
2023-09-09 15:50:28 -0300

    removendo o comportamento padrão do formulario

--- src/Pages/PageLogin/index.jsx
+++ src/Pages/PageLogin/index.jsx
@@ -5,13 +5,14 @@ import { UsuarioContext } from "../../Contexts/UsuarioContext";
 const Login = () => {
     const { nomeUsuario, setNomeUsuario, senhaUsuario, setSenhaUsuario } = useContext(UsuarioContext);
     return (
-        <section>
+        <form>
             <h2>Login</h2>
             <h2>
                 Úsuario:
             </h2>
             <input type="text" placeholder={`use: marciomartins12`}
                 value={nomeUsuario}
+                required={true}
                 onChange={(evento) =>
                     setNomeUsuario(evento.target.value)
                 }
@@ -22,11 +23,14 @@ const Login = () => {
             </h3>
             <input type="text" placeholder={`use: 1234`}
              value={senhaUsuario}
+             required={true}
              onChange={(evento) =>
                  setSenhaUsuario(evento.target.value)
              } />
-             <button>Fazer login</button>
-        </section>
+             <button
+             onClick={(event)=>{event.preventDefault()}}
+             >Fazer login</button>
+        </form>
     );
 };
 export default Login;
