e4d2073f7bce641d7bb11f04f0b2d1f7114a1d51
Marcio Martins <mmarciom1martins635@gmail.com>
2023-09-12 16:10:22 -0300

    .

--- src/Pages/CarrinhoDeCompras/index.jsx
+++ src/Pages/CarrinhoDeCompras/index.jsx
@@ -13,6 +13,11 @@ const CarrinhoDeCompras = () => {
             <span>adicionar algo?</span>
             </div>
             <span>escolher metade</span>
+            {/* quero colocar as pizzas dentro desse select para cada select a pizza pegue a
+             metade da outra e junte com a que esta na tela selecionada la do carrinho. 
+            e ao selection a pizza uma imagem da pizza seleciona cobre metade da pizza que ja estava no carrinho assim deixando uma pizza
+            com duas metades e isso tudo tem que ficar salvo la no carrinho. trabalho para fazer amanha
+            enviar item para o carrinho*/}
             <select>
                 <option name="refri" id="">pizza</option>
                 <option name="refri" id="">pizza q</option>
