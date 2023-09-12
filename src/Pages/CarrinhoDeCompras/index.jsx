bc7a99c943d743639c4604a8c4d6dd2b4d776a22
Marcio Martins <mmarciom1martins635@gmail.com>
2023-09-12 16:07:00 -0300

    comecando a page do carringo mas sem nenhum item sendo adicionado no carrinho apenas fazendo a page ora ver como vai ficar

--- src/Pages/CarrinhoDeCompras/index.jsx
+++ src/Pages/CarrinhoDeCompras/index.jsx
@@ -1,9 +1,26 @@
-import style from "./CarrinhoDeCompras.module.css"
-const CarrinhoDeCompras = ()=> {
+import style from "./CarrinhoDeCompras.module.css";
+import img from "../../../public/imagem/1.png";
+
+const CarrinhoDeCompras = () => {
     return (
         <section className={style.section}>
-item???????????????
+            <div className={style.item}>
+
+            <img src={img} className={style.img} alt="nome" />
+            <div>
+
+            <h6>nome da pizza</h6>
+            <span>adicionar algo?</span>
+            </div>
+            <span>escolher metade</span>
+            <select>
+                <option name="refri" id="">pizza</option>
+                <option name="refri" id="">pizza q</option>
+                <option name="refri" id="">aaaaaaaaa1</option>
+            </select>
+            </div>
         </section>
+
 
     )
 }
